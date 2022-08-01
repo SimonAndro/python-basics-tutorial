ff42360b9831892706681ecb38b251cc1f25a649
Simon <41785978+SimonAndro@users.noreply.github.com>
2022-08-01 21:25:05 +0800

    chore: file writing and reading

---
+++ basic task manager/main.py
@@ -0,0 +1,27 @@
+
+
+
+filename = "tasks.txt"
+
+# file = open(file=filename,mode='a')
+# file.write("this is my first file->1\n")
+# file.write("this is my first file->2\n")
+# file.write("this is my first file->3\n")
+# file.close()
+
+# file = open(file=filename, mode='r')
+# for i in range(1,4):
+#     str = file.readline()
+#     print(str)
+# file.close()
+
+##getting input from the console
+
+print("enter something: ",end="")
+
+str = str(input())
+
+print(str)
+
+##task 2, get 5 names  from the console and save
+##them in a file
